9577a3ea3aeca9b96bd9af7c5aeda3ab9323f6e3
sig-ume <beyondthemoonface@gmail.com>
2022-11-17 22:07:30 +0900

    セキュリティポリシーリンクのテストを追加

---
+++ app/src/androidTest/java/com/fias/ddrhighspeed/NavigationDrawerTest.kt
@@ -0,0 +1,122 @@
+@file:Suppress("DEPRECATION")
+
+package com.fias.ddrhighspeed
+
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
+import android.net.Uri
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.core.app.ApplicationProvider.*
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.*
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.not
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class NavigationDrawerTest {
+
+    private val url: Uri = Uri.parse("https://fia1988.github.io/PrivacyPolicy")
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @get:Rule
+    val intentsTestRule = IntentsTestRule(MainActivity::class.java)
+
+    @Before
+    fun stubAllExternalIntents() {
+        // By default Espresso Intents does not stub any Intents. Stubbing needs to be setup before
+        // every test run. In this case all external Intents will be blocked.
+        intending(not(isInternal())).respondWith(
+            Instrumentation.ActivityResult(
+                Activity.RESULT_OK,
+                null
+            )
+        )
+    }
+
+    @Test
+    fun mainActivityTest() {
+        val toolBarLeftButton = onView(
+            allOf(
+                withContentDescription("Open navigation drawer"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.toolbar),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        toolBarLeftButton.perform(click())
+
+        val privacyPolicyLink = onView(
+            allOf(
+                withId(R.id.security_policy),
+                childAtPosition(
+                    allOf(
+                        withId(com.google.android.material.R.id.design_navigation_view),
+                        childAtPosition(
+                            withId(R.id.nav_view),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        privacyPolicyLink.perform(click())
+
+        intended(
+            allOf(
+                hasAction(Intent.ACTION_VIEW),
+                hasData(url)
+            )
+        )
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
