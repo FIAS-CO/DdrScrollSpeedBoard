410d1a8a78365b59c164a3d0b96c3c9428fc61f0
sig-ume <beyondthemoonface@gmail.com>
2023-05-29 14:39:40 +0900

    KotlinMultiplatformで不要になったDB系クラスの削除

--- app/src/main/java/com/fias/ddrhighspeed/database/SongApplication.kt
+++ app/src/main/java/com/fias/ddrhighspeed/database/SongApplication.kt
@@ -5,7 +5,5 @@ import com.fias.ddrhighspeed.shared.cache.Database
 import com.fias.ddrhighspeed.shared.cache.DatabaseDriverFactory
 
 class SongApplication : Application() {
-    // TODO: 使わないので消す
-    val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
     val db: Database by lazy { Database(DatabaseDriverFactory(this)) }
 }
