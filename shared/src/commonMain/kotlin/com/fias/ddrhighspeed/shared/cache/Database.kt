113343b6e301f999784f6e1c8d3ab813393e76c0
sig-ume <beyondthemoonface@gmail.com>
2023-08-20 01:48:52 +0900

    DBから検索キーワードでSelectするメソッドが不要になったので削除

--- shared/src/commonMain/kotlin/com/fias/ddrhighspeed/shared/cache/Database.kt
+++ shared/src/commonMain/kotlin/com/fias/ddrhighspeed/shared/cache/Database.kt
@@ -5,10 +5,6 @@ class Database(databaseDriverFactory: DatabaseDriverFactory) : IDatabase {
     private val database = AppDatabase(driver)
     private val dbQuery = database.appDatabaseQueries
 
-    override fun searchSongsByName(searchWord: String): List<Song> {
-        return dbQuery.getByNameContainWord(searchWord).executeAsList()
-    }
-
     override fun getNewSongs(): List<Song> {
         return dbQuery.getNew().executeAsList()
     }
