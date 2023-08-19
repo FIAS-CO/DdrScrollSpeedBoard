df44aa2957f758bc5eebc64e10f37b855571e135
sig-ume <beyondthemoonface@gmail.com>
2023-08-19 23:49:32 +0900

    検索画面のRecyclerViewの更新をリストのLiveData連動で行うよう修正し、画面初期化時の処理を整理

--- app/src/main/java/com/fias/ddrhighspeed/search/songsearch/EstimateByNameFragment.kt
+++ app/src/main/java/com/fias/ddrhighspeed/search/songsearch/EstimateByNameFragment.kt
@@ -3,8 +3,6 @@ package com.fias.ddrhighspeed.search.songsearch
 import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,9 +21,7 @@ import com.fias.ddrhighspeed.data.DataVersionDataStore
 import com.fias.ddrhighspeed.database.SongApplication
 import com.fias.ddrhighspeed.databinding.FragmentEstimateByNameBinding
 import com.fias.ddrhighspeed.shared.spreadsheet.SpreadSheetService
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 private const val version_PREFERENCES_NAME = "version_preferences"
 val Context.versionDataStore: DataStore<Preferences> by preferencesDataStore(
@@ -33,7 +29,7 @@ val Context.versionDataStore: DataStore<Preferences> by preferencesDataStore(
 )
 
 class EstimateByNameFragment : Fragment() {
-    private val handler: Handler = Handler(Looper.getMainLooper())
+//    private val handler: Handler = Handler(Looper.getMainLooper())
     private var _fragmentBinding: FragmentEstimateByNameBinding? = null
     private val binding get() = _fragmentBinding!!
     private val spreadSheetService = SpreadSheetService()
@@ -78,35 +74,20 @@ class EstimateByNameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        lifecycleScope.launch {
-            val newSongs = withContext(Dispatchers.IO) {
-                viewModel.getNewSongs()
-            }
-            handler.post {
-                searchedSongsAdapter.submitList(newSongs)
-            }
-
-            val dataVersion = versionDataStore.getDataVersion()
-            if (dataVersion == 0) {
-                refreshDataAndView()
-            } else {
-                viewModel.checkNewDataVersionAvailable(dataVersion)
-            }
-        }
-
         binding.searchedSongs.adapter = searchedSongsAdapter
 
         viewModel.searchWord.observe(viewLifecycleOwner) {
-            lifecycleScope.launch {
-                updateTableBySearchWord()
+            viewModel.setSongDataListBySearchWord()
+        }
+
+        viewModel.songDataList.observe(viewLifecycleOwner) { songDataList ->
+            searchedSongsAdapter.submitList(songDataList) {
+                binding.searchedSongs.scrollToPosition(0)
             }
         }
 
         viewModel.localDataVersion.observe(viewLifecycleOwner) { version ->
             binding.dataVersion.text = getString(R.string.display_version, version.toString())
-            lifecycleScope.launch {
-            }
         }
 
         viewModel.updateAvailable.observe(viewLifecycleOwner) { updateAvailable ->
@@ -141,6 +122,16 @@ class EstimateByNameFragment : Fragment() {
                 viewModel.checkNewDataVersionAvailable(versionDataStore.getDataVersion())
             }
         }
+
+        lifecycleScope.launch {
+            val dataVersion = versionDataStore.getDataVersion()
+            if (dataVersion == 0) {
+                refreshDataAndView()
+            } else {
+                viewModel.checkNewDataVersionAvailable(dataVersion)
+                viewModel.setSongDataListBySearchWord()
+            }
+        }
     }
 
     private suspend fun refreshDataAndView() {
@@ -148,8 +139,6 @@ class EstimateByNameFragment : Fragment() {
         viewModel.localDataVersion.value?.let {
             versionDataStore.saveDataVersionStore(it)
         }
-
-        updateTableBySearchWord()
     }
 
     private fun switchLoading(isLoading: Boolean) {
@@ -161,16 +150,5 @@ class EstimateByNameFragment : Fragment() {
     private fun switchUpdateAvailable(updateAvailable: Boolean) {
         binding.dataVersion.visibility = if (updateAvailable) View.GONE else View.VISIBLE
         binding.updateButton.visibility = if (updateAvailable) View.VISIBLE else View.GONE
-    }
-
-    private suspend fun updateTableBySearchWord() {
-        val songs = withContext(Dispatchers.IO) {
-            viewModel.searchSongsByName()
-        }
-        handler.post {
-            searchedSongsAdapter.submitList(songs) {
-                binding.searchedSongs.scrollToPosition(0)
-            }
-        }
     }
 }
