7e67c610839cd98bc86e8587cec2606f294d2e7b
sig-ume <beyondthemoonface@gmail.com>
2023-08-20 01:48:52 +0900

    DBから検索キーワードでSelectするメソッドが不要になったので削除

--- shared/src/commonMain/kotlin/com/fias/ddrhighspeed/shared/cache/IDatabase.kt
+++ shared/src/commonMain/kotlin/com/fias/ddrhighspeed/shared/cache/IDatabase.kt
@@ -2,7 +2,6 @@ package com.fias.ddrhighspeed.shared.cache
 
 interface IDatabase {
 
-    fun searchSongsByName(searchWord: String): List<Song>
     fun getNewSongs(): List<Song>
     fun reinitializeSongNames(songNames: List<SongName>)
     fun reinitializeShockArrowExists(shockArrowExists: List<ShockArrowExists>)
