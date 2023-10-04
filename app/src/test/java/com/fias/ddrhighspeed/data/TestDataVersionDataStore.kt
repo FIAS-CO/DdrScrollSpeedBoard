97148e1b21a353480aede76e9b4694ab646e6b73
sig-ume <beyondthemoonface@gmail.com>
2023-10-04 23:35:10 +0900

    テスト作成中

---
+++ app/src/test/java/com/fias/ddrhighspeed/data/TestDataVersionDataStore.kt
@@ -0,0 +1,11 @@
+package com.fias.ddrhighspeed.data
+
+class TestDataVersionDataStore: IDataVersionDataStore {
+    override suspend fun saveDataVersionStore(topRowIndex: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getDataVersion(): Int {
+        TODO("Not yet implemented")
+    }
+}
