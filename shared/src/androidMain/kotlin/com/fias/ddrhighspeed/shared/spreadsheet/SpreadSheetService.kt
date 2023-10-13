87ff576fa546e38a0b84885c1a3ecd04da64bac0
sig-ume <beyondthemoonface@gmail.com>
2023-10-13 21:01:46 +0900

    SpreadsheetのURLを製品版に変更

--- shared/src/androidMain/kotlin/com/fias/ddrhighspeed/shared/spreadsheet/SpreadSheetService.kt
+++ shared/src/androidMain/kotlin/com/fias/ddrhighspeed/shared/spreadsheet/SpreadSheetService.kt
@@ -7,6 +7,6 @@ actual class SpreadSheetService : ISpreadSheetService {
     actual override fun getHttpClient(): HttpClient = HttpClient(CIO) { expectSuccess = true }
 
     override fun getUrlBase(): String =
-//        "https://docs.google.com/spreadsheets/d/1HA8RH2ozKQTPvvq2BVcVoOSEMVIldRw89tFtNg8Z4V8/export?format=tsv&gid="
-        "https://docs.google.com/spreadsheets/d/1W1FDy3a0Ty1US3RPohSR_rlraBq2xON9q2yUbOfHZIQ/export?format=tsv&gid="
+        "https://docs.google.com/spreadsheets/d/1HA8RH2ozKQTPvvq2BVcVoOSEMVIldRw89tFtNg8Z4V8/export?format=tsv&gid="
+//        "https://docs.google.com/spreadsheets/d/1W1FDy3a0Ty1US3RPohSR_rlraBq2xON9q2yUbOfHZIQ/export?format=tsv&gid="
 }
