3b99dd1716fadcade4c7acf09d2c4f0cbcf3da02
sig-ume <beyondthemoonface@gmail.com>
2023-10-22 20:46:32 +0900

    iOSで使うためにSongData,CourseDataおよびその変換クラスをshareにコピー
    - Android側に残っているものは後で消すこと

---
+++ shared/src/commonMain/kotlin/com/fias/ddrhighspeed/shared/model/SongData.kt
@@ -0,0 +1,38 @@
+package com.fias.ddrhighspeed.shared.model
+
+// TODO Android側にあるものをこちらに置き換える
+data class SongData(
+    val id: Long,
+    val name: String,
+    val composer: String?,
+    val version: String,
+    val displayBpm: String,
+    val baseBpm: Double,
+    val subBpm: Double?,
+    val minBpm: Double?,
+    val maxBpm: Double?,
+    val besp: Long,
+    val bsp: Long,
+    val dsp: Long,
+    val esp: Long,
+    val csp: Long,
+    val bdp: Long,
+    val ddp: Long,
+    val edp: Long,
+    val cdp: Long,
+    val shockArrow: String?,
+    val deleted: Long,
+    val difficultyLabel: String
+) {
+    fun nameWithDifficultyLabel(): String =
+        name + if (difficultyLabel.isNotEmpty()) "(${difficultyLabel})" else ""
+
+    fun hasHighSpeedArea() = hasMaxBpm() || hasSubBpm()
+    fun hasLowSpeedArea() = hasValue(minBpm)
+
+    private fun hasMaxBpm() = hasValue(maxBpm)
+    private fun hasSubBpm() = hasValue(subBpm)
+
+    private fun hasValue(value: Double?) = value?.let { it > 0.0 } ?: false
+
+}
