ffc761248d5a81bc003278e77628333b4680f4dd
sig-ume <beyondthemoonface@gmail.com>
2025-11-04 01:56:45 +0900

    諸々内部的なバージョンアップ

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,18 +2,12 @@ plugins {
     kotlin("multiplatform")
     id("com.android.library")
 
-    kotlin("plugin.serialization") version "1.8.10"
+    kotlin("plugin.serialization")
     id("com.squareup.sqldelight")
 }
 
 kotlin {
-    android {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "1.8"
-            }
-        }
-    }
+    androidTarget()
     
     listOf(
         iosX64(),
@@ -25,7 +19,7 @@ kotlin {
         }
     }
 
-    val coroutinesVersion = "1.6.4"
+    val coroutinesVersion = "1.10.1"
     val ktorVersion = "2.3.13"
     val sqlDelightVersion = "1.5.5"
 
@@ -56,11 +50,6 @@ kotlin {
         val iosArm64Main by getting
         val iosSimulatorArm64Main by getting
         val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-
             dependencies {
                 implementation("io.ktor:ktor-client-darwin:$ktorVersion")
                 implementation("com.squareup.sqldelight:native-driver:$sqlDelightVersion")
@@ -70,10 +59,6 @@ kotlin {
         val iosArm64Test by getting
         val iosSimulatorArm64Test by getting
         val iosTest by creating {
-            dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }
@@ -83,6 +68,11 @@ android {
     compileSdk = 34
     defaultConfig {
         minSdk = 24
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
